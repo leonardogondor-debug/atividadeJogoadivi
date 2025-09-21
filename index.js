e7ab7c9eb0216fce7a35cf994ca5a55e68f97c19
leovit <leonardogondor@gmail.com>
2025-09-21 01:20:15 -0300

    contador nao esta funcionando

--- index.js
+++ index.js
@@ -16,13 +16,45 @@ const valorMaior = (chute) => chute > 100;
 
 function jogo() {
     const chuteUsuario = valorUsuario();
-
+    let valorJogo = valorSorteado();
+    alert(valorJogo);
+    
+    /*estrutura de repeticao*/
+    while (cont >= 1) {
+    /*se valor errado nega jogada  */
     if(valorNegativo(chuteUsuario)) {
-        alert("valor negativo invalido ");
-        return;
+        alert("valor menor que 1 nao permitido");
+        continue;
     }
     if(valorMaior(chuteUsuario)) {
         alert("valor maior que 100 nao permitido");
-        return;
+        continue;
     }
+    
+    /*checa vitoria da dicas */
+    if(valorJogo == chuteUsuario) {
+        document.getElementById('resultado').innerHTML="Voce acertou.";
+        cont--
+        document.getElementById('tentativas').innerHTML=`Numero de tentativas: ${cont}`;
+        break;
+    }
+    else if(valorJogo != chuteUsuario) {
+        if(valorJogo > chuteUsuario) {
+            document.getElementById('resultado').innerHTML="E um valor maior";
+            cont--
+            document.getElementById('tentativas').innerHTML=`Numero de tentativas: ${cont}`;
+        }
+        else if(valorJogo < chuteUsuario) {
+            document.getElementById('resultado').innerHTML="E um valor menor";
+            cont--
+            document.getElementById('tentativas').innerHTML=`Numero de tentativas: ${cont}`;
+        }
+    }
+
+    if(cont == 1) {
+        alert('sua antativas acabaram');
+        break;
+    }
+    }
+    
 }
