03fdffb81f6cbae5f2bb9139c91615e7cbb769dc
leovit <leonardogondor@gmail.com>
2025-09-21 00:18:03 -0300

    validacao do valor do usuario if

--- index.js
+++ index.js
@@ -7,4 +7,22 @@ function valorSistema (min, max) {
 
 /*armazena o valor do sistema*/
 const valorSorteado = () => valorSistema(1, 100);
+/*chutes do usuario armazena*/
+const valorUsuario = () => document.getElementById("chute").value;
 
+/*valisa valor do usuario*/
+const valorNegativo = (chute) =>  chute < 1;
+const valorMaior = (chute) => chute > 100;
+
+function jogo() {
+    const chuteUsuario = valorUsuario();
+
+    if(valorNegativo(chuteUsuario)) {
+        alert("valor negativo invalido ");
+        return;
+    }
+    if(valorMaior(chuteUsuario)) {
+        alert("valor maior que 100 nao permitido");
+        return;
+    }
+}
