968dc11edb4c2043cbdde76d6a30930a94265453
leovit <leonardogondor@gmail.com>
2025-09-22 02:30:52 -0300

    tudo finalizado

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
-/*escolhe o valor do sistema*/ 
-function valorSistema (min, max) {
-  min = Math.ceil(1)
-  max = Math.floor(100)
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+/*escolhe o valor do sistema*/
+const valorSortiado = () => Math.floor(Math.random() * 100) + 1;
+let valorSecreto = valorSortiado();
+let tentativa = 10 ;
 
-/*armazena o valor do sistema*/
-const valorSorteado = () => valorSistema(1, 100);
+
+
 /*chutes do usuario armazena*/
 const valorUsuario = () => document.getElementById("chute").value;
 
@@ -14,15 +12,55 @@ const valorUsuario = () => document.getElementById("chute").value;
 const valorNegativo = (chute) =>  chute < 1;
 const valorMaior = (chute) => chute > 100;
 
-function jogo() {
-    const chuteUsuario = valorUsuario();
+/**reseta o jogo */
+function resetarJogo () {
+    valorSecreto = valorSortiado();
+    tentativa = 10;
+    document.getElementById('resultado').innerHTML='Dicas resultados';
+    document.getElementById('tentativas').innerHTML='Numero de tentativas: 10';
+    document.getElementById('chute').value = '';
+    document.getElementById('chute').disabled = false;
+}
 
+function jogo() {
+    const chuteUsuario = Number(valorUsuario());
+   
     if(valorNegativo(chuteUsuario)) {
         alert("valor negativo invalido ");
         return;
     }
     if(valorMaior(chuteUsuario)) {
         alert("valor maior que 100 nao permitido");
+        return;
+    }
+
+        if (chuteUsuario === valorSecreto) {
+            document.getElementById('resultado').innerHTML=`Voce venceu numero secreto: ${valorSecreto}`;
+            document.getElementById('chute').disabled = true;
+            setTimeout(resetarJogo, 2000);
+            return;
+        }
+        
+        tentativa--;
+
+        if (chuteUsuario !== valorSecreto) {
+            if (chuteUsuario < valorSecreto) {
+                document.getElementById('resultado').innerHTML='Errado e um valor maior';
+                document.getElementById('tentativas').innerHTML=`Numero de tentativas: ${tentativa}`;
+                
+            }
+
+            else if (chuteUsuario > valorSecreto) {
+                document.getElementById('resultado').innerHTML='Errado e um valor menor';
+                document.getElementById('tentativas').innerHTML=`Numero de tentativas: ${tentativa}`;
+            
+            }
+        }
+        
+    if (tentativa == 0) {
+        document.getElementById('resultado').innerHTML=`Voce perdeu valor secreto era: ${valorSecreto}`;
+        document.getElementById('chute').disabled = true;
+        setTimeout(resetarJogo, 2000);
         return;
     }
 }
