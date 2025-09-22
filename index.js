9f512b45447c5990a7e9a455e5a20ecfb756115c
leovit <leonardogondor@gmail.com>
2025-09-21 22:54:02 -0300

    JS 2 tentativa

--- index.js
+++ index.js
@@ -16,45 +16,13 @@ const valorMaior = (chute) => chute > 100;
 
 function jogo() {
     const chuteUsuario = valorUsuario();
-    let valorJogo = valorSorteado();
-    alert(valorJogo);
-    
-    /*estrutura de repeticao*/
-    while (cont >= 1) {
-    /*se valor errado nega jogada  */
+
     if(valorNegativo(chuteUsuario)) {
-        alert("valor menor que 1 nao permitido");
-        continue;
+        alert("valor negativo invalido ");
+        return;
     }
     if(valorMaior(chuteUsuario)) {
         alert("valor maior que 100 nao permitido");
-        continue;
+        return;
     }
-    
-    /*checa vitoria da dicas */
-    if(valorJogo == chuteUsuario) {
-        document.getElementById('resultado').innerHTML="Voce acertou.";
-        cont--
-        document.getElementById('tentativas').innerHTML=`Numero de tentativas: ${cont}`;
-        break;
-    }
-    else if(valorJogo != chuteUsuario) {
-        if(valorJogo > chuteUsuario) {
-            document.getElementById('resultado').innerHTML="E um valor maior";
-            cont--
-            document.getElementById('tentativas').innerHTML=`Numero de tentativas: ${cont}`;
-        }
-        else if(valorJogo < chuteUsuario) {
-            document.getElementById('resultado').innerHTML="E um valor menor";
-            cont--
-            document.getElementById('tentativas').innerHTML=`Numero de tentativas: ${cont}`;
-        }
-    }
-
-    if(cont == 1) {
-        alert('sua antativas acabaram');
-        break;
-    }
-    }
-    
 }
